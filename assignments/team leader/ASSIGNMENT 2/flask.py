527bea10714f357f9b87a796b8aa51a5380c498c
sidhupalanikumar <114793706+sidhupalanikumar@users.noreply.github.com>
2022-10-15 14:35:27 +0530

    Add files via upload

---
+++ assignments/team leader/ASSIGNMENT 2/flask.py
@@ -0,0 +1,24 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
